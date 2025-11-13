f6ee60d58fd1d9281970f87a7b848b1c05ba8b67
shwetasnprabhu-code <shwetasnprabhu@gmail.com>
2025-11-13 11:37:55 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,15 +4,15 @@ from llama_cpp import Llama
 import streamlit as st  # ADD THIS LINE
 import os
 if not os.path.exists("phi3.gguf"):
-    print("Downloading Phi-3 GGUF (2.4GB)... This takes ~5 minutes.")
+    print("Downloading Phi-3 GGUF (2.2GB)... This takes ~5 minutes.")
     from huggingface_hub import hf_hub_download
     hf_hub_download(
-        repo_id="TheBloke/Phi-3-mini-4k-instruct-GGUF",
-        filename="phi-3-mini-4k-instruct.Q4_K_M.gguf",  # CORRECT FILE
+        repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
+        filename="Phi-3-mini-4k-instruct-q4.gguf",  # Correct filename
         local_dir=".",
         local_dir_use_symlinks=False
     )
-    os.rename("phi-3-mini-4k-instruct.Q4_K_M.gguf", "phi3.gguf")
+    os.rename("Phi-3-mini-4k-instruct-q4.gguf", "phi3.gguf")
     print("Model downloaded and ready!")
 
 @st.cache_resource
