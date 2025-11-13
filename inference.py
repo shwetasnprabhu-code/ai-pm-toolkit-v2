d0d584318a00b155fbabd4823d0a269326201689
shwetasnprabhu-code <shwetasnprabhu@gmail.com>
2025-11-13 15:35:05 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ if not os.path.exists("phi3.gguf"):
     from huggingface_hub import hf_hub_download
     hf_hub_download(
         repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
-        filename="Phi-3-mini-4k-instruct-q4_0.gguf",
+        filename="Phi-3-mini-4k-instruct-q4.gguf",
         local_dir=".",
         local_dir_use_symlinks=False
     )
